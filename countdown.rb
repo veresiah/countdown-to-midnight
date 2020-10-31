06e45ad1241a8d2b342d46dfa21fa0e8a13272bb
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-10-31 14:50:28 -0400

    commit tree 63ad3ebc3b35c7891010506ab3fb1db13240b92b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    while seconds > 0 
+         puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    end 
+
+      "HAPPY NEW YEAR!"
+end 
+
+
+def countdown_with_sleep(seconds) 
+    while seconds > 0
+        sleep(1)
+    seconds -= 1
+    end 
 end
+
+
+
